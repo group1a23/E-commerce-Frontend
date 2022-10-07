ed1f52912db3f5d21d6cf5609c6d2c318e974e3d
rayen <94569316+kaaboor@users.noreply.github.com>
2022-10-07 01:01:52 +0100

    init

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,26 +3,26 @@ import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import Rating from "./Rating"
 
-const Product = ({ product }) => {
+const Team = ({ team }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Link to={`/team/${product._id}`}>
+      <Link to={`/team/${team._id}`}>
         <Card.Img src={product.image} variant="top"></Card.Img>
       </Link>
 
       <Card.Body>
-        <Link to={`/team/${product._id}`}>
+        <Link to={`/team/${team._id}`}>
           <Card.Title as="div">
-            <strong>{product.name}</strong>{" "}
+            <strong>{team.name}</strong>{" "}
           </Card.Title>
         </Link>
 
         <Card.Text as="h3">
-          <strong>${product.price}</strong>{" "}
+          <strong>${team.price}</strong>{" "}
         </Card.Text>
       </Card.Body>
     </Card>
   )
 }
 
-export default Product
+export default Team
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import products from "../products"
+import teamm from "../teamm"
 import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
-
+import Team from "../components/Team"
 const Home = () => {
   return (
     <>
       <h1>The Lastest Products</h1>
       <Row>
-        {products.map((product, idx) => (
+        {teamm.map((team, idx) => (
           <Col key={idx} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
+            <Team team={team} />
           </Col>
         ))}
       </Row>
